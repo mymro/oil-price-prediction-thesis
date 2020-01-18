a3eda6160b1a1266478541fe5a3707a79749e764
Constantin <constantin.budin@gmail.com>
2020-01-18 11:44:04 +0900

    initial commit

---
+++ scraper/upi.js
@@ -0,0 +1,140 @@
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer-extra');
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+const blockResourcesPlugin = require('puppeteer-extra-plugin-block-resources')({
+    blockedTypes: new Set(['image', 'stylesheet', 'media', 'font'])
+  })
+const PromisePool = require('es6-promise-pool');
+const genericPool = require("generic-pool");
+const winston = require("winston");
+const moment = require('moment-timezone');
+const waitUntil = require('async-wait-until');
+const fs = require('fs');
+const { combine, timestamp, label, printf } = winston.format;
+const db_helper = require('./upi_db');
+
+const myFormat = printf(({ level, message}) => {
+    return `${moment().format("YYYY-MM-DD HH:mm:ss.SSS")} ${level}: ${message}`;
+  });
+
+const logger = winston.createLogger({
+    format: myFormat,
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'upi.log'})
+    ]
+  });
+
+const max_pages = 5;
+let current_page = 0;
+let running_article_queries = 0;
+
+function generateWaitCondition(running_queries){
+    return(()=>{
+        return running_article_queries < running_queries
+    })
+}
+
+async function getNewArticles(page_pool){
+    running_article_queries += 1;
+    const my_task = db_helper.get_search_task();
+
+    const page = await page_pool.acquire();
+    let nodes;
+    logger.info(`Getting articles for ${my_task.search_term} page:${my_task.page}`);
+    await page.goto(`https://www.upi.com/search/?s_l=articles&ss=${my_task.search_term}&s_term=ea&offset=${my_task.page}`);
+    const $ = cheerio.load(await page.content());
+    nodes = $("div[class='row story list'] > .col-md-12")
+    if(nodes.length === 0){
+        logger.info(`Articles for ${my_task.search_term} ended on page ${my_task.page}. Jumping to next search term`)
+        db_helper.set_search_task_done(my_task.id);
+    }else{
+        nodes.each(function(i, elem){
+            let link = $(this).find('a').attr('href');
+            let url = link.slice(0, link.lastIndexOf("/"));
+            logger.debug(`found: ${url}`);
+
+            db_helper.add_article(my_task, url);
+        })
+    }
+    db_helper.page_done(my_task, page)
+    page_pool.release(page);
+    running_article_queries -= 1;
+}
+
+async function fetchNextArticle(page_pool){
+    const article = db_helper.get_article();
+    const page = await page_pool.acquire();
+    logger.info(`Fetching article from: ${article.url}`);
+    await page.goto(article.url);
+    const $ = cheerio.load(await page.content());
+    article.title = $("div.news-head > h1.headline").text();
+    article.date = moment.tz($("div.news-head > div.montserrat > div.article-date").text().trim(), "MMM. D, YYYY / h:m a", "Etc/GMT-5").valueOf();
+    article.filename = article.date+"_"+article.title.replace(/[\s\\\/\*:\?\"\<\>]+/g, "").substr(0, 10)+".txt";
+    const file = fs.createWriteStream(`./upi_articles/${article.filename}`);
+    $("article[itemprop='articleBody'] > p").each(function(i, elem){
+        file.write($(this).text().replace(/[\r\n]+/g, ""));
+    });
+    if(article.date < article.min_date){
+        db_helper.set_search_task_done(article.search_term);
+    }
+    db_helper.article_downloaded(article);
+    page_pool.release(page);
+}
+
+let chrome = null;
+
+logger.info("launching headless browser");
+puppeteer.use(blockResourcesPlugin);
+//puppeteer.use(AdblockerPlugin());
+puppeteer.launch({
+    headless:true,
+    userDataDir:"./chrome"
+}).then(browser=>{
+
+    chrome = browser;
+    const factory = {
+        create: function() {
+            logger.debug("opening new page");
+            async function gen(){
+                let page = await browser.newPage();
+                page.setDefaultTimeout(120000);
+                return(page);
+            }
+            return gen();
+        },
+        destroy: function(page) {
+            return page.close();
+        }
+    };
+    return genericPool.createPool(factory, {min:1, max:max_pages});
+
+}).then(page_pool=>{
+    function promiseProducer(){
+        if(db_helper.waiting_articles() < 5
+            && db_helper.has_search_tasks()
+            && running_article_queries < 1){
+
+            return getNewArticles(page_pool);
+        }else if(db_helper.waiting_articles() == 0 
+            && running_article_queries > 0){
+            
+            logger.info("waiting for new articles");
+            return waitUntil(generateWaitCondition(running_article_queries), 120000).catch();
+        }else if(db_helper.waiting_articles() > 0){
+
+            return fetchNextArticle(page_pool);
+        }else{
+            return null;
+        }
+    }
+
+    let promise_pool = new PromisePool(promiseProducer, max_pages);
+    return promise_pool.start();
+}).then(()=>{
+    logger.info("done");
+}).catch(error=>{
+    logger.error(error);
+}).finally(()=>{
+    chrome.close();
+})
---
+++ scraper/upi_db.js
@@ -0,0 +1,87 @@
+const db = require('better-sqlite3')('upi.sqlite3');
+const moment = require('moment');
+const Queue = require('queue-fifo');
+
+class db_helper{
+    constructor(){
+        this.update_tasks();
+        this.update_articles();
+    }
+
+    update_tasks(){
+        let task_arr = db.prepare("SELECT * FROM config WHERE done <> 1").all();
+        this.search_tasks = {};
+        this.search_tasks_page_helper = {};
+        for(let elem in task_arr){
+            this.search_tasks[task_arr[elem].id] = task_arr[elem];
+            this.search_tasks_page_helper[task_arr[elem].id] = {page: task_arr[elem].page, waiting:[]}
+        }
+    }
+
+    update_articles(){
+        this.articles = new Queue;
+        let articles_arr = db.prepare("SELECT articles.*, config.min_date AS min_date FROM articles LEFT JOIN config ON config.id = articles.search_term WHERE articles.fetched <> 1 ORDER BY articles.id ASC").all();
+        for(let elem in articles_arr){
+            this.articles.enqueue(articles_arr[elem]);
+        }
+    }
+
+    waiting_articles(){
+        return this.articles.size();
+    }
+
+    get_article(){
+        return this.articles.dequeue();
+    }
+
+    article_downloaded(article){
+        db.prepare("UPDATE articles SET title = ?, date = ?, filename = ?, fetched = 1 WHERE url = ?")
+            .run(article.title, article.date, article.filename, article.url);
+    }
+
+    has_search_tasks(){
+        return Object.keys(this.search_tasks).length > 0;
+    }
+
+    get_search_task(){
+        const id = Object.keys(this.search_tasks)[0];
+        this.search_tasks[id].page += 1;
+        return this.search_tasks[id];
+    }
+
+    set_search_task_done(id){
+        db.prepare("UPDATE config SET done = 1 WHERE id = ?").run(id);
+        delete this.search_tasks[id];
+    }
+
+    check_wating_pages(task){
+        if(this.search_tasks_page_helper[task.id].waiting.length > 0){
+            this.search_tasks_page_helper[task.id].waiting.sort(function(a, b){return a-b});
+            while(this.search_tasks_page_helper[task.id].page+1 === this.search_tasks_page_helper[task.id].waiting[0]){
+                this.search_tasks_page_helper[task.id].page += 1;
+                this.search_tasks_page_helper[task.id].waiting.shift();
+            }
+        }
+    }
+
+    page_done(task){
+        if(this.search_tasks_page_helper[task.id].page + 1 === task.page){
+            this.search_tasks_page_helper[task.id].page += 1;
+            this.check_wating_pages(task);
+            db.prepare("UPDATE config SET page = ? WHERE id = ?").run(this.search_tasks_page_helper[task.id].page, task.id);
+        }else if(this.search_tasks_page_helper[task.id].page + 1 < task.page){
+            his.search_tasks_page_helper[task.id].waiting.push(task.page);
+        }
+    }
+
+    add_article(task, url){
+        try {
+            db.prepare("INSERT INTO articles(url, search_term) VALUES(?, ?)").run(url, task.id);
+            this.articles.enqueue({url:url, search_term: task.id, min_date: task.min_date});
+        } catch (error) {
+            console.log("error");
+        }
+    }
+}
+
+module.exports = new db_helper();
